40b3d21eead8e17be7dac38afbc507ea0804c568
David Campo <david.campo@avature.net>
2017-01-01 23:51:28 -0300

    1001 uri extremely basic

---
+++ uri/1001/extremely_basic.cpp
@@ -0,0 +1,19 @@
+/*
+Title: Extremely Basic
+Tags: adhoc, basic
+URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+Resources of interest:
+Solver: David N. Campo
+Contact e-mail: dncampo at gmail dot com
+Solution remarks: none.
+*/
+
+#include <iostream>
+using namespace std;
+compare_substring
+int main() {
+	int a,b;
+	cin >> a >> b;
+	cout << "X = " << a+b << endl;
+	return 0;
+}
