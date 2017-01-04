f7f3ca3ddeb821aed15b2d943207bc919e505fda
David N Campo <dncampo@gmail.com>
2017-01-03 23:27:17 -0300

    uri 1004 simple product

---
+++ uri/1004/simple_product.cpp
@@ -0,0 +1,21 @@
+/*
+Title: Simple Product
+Tags: adhoc, basic
+URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+Resources of interest:
+Solver: David N. Campo
+Contact e-mail: dncampo at gmail dot com
+Solution remarks: none.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b;
+	cin >> a >> b;
+
+	cout << "PROD = " << a*b << endl;
+	return 0;
+}
