014f9c54e9f730187015c5a11191a5defa761a75
David N Campo <dncampo@gmail.com>
2017-01-05 00:53:57 -0300

    uri 1805 Natural Sum

---
+++ uri/1805/natural_sum.cpp
@@ -0,0 +1,21 @@
+/*
+Title: Natural Sum
+Tags: adhoc, basic
+URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1805
+Resources of interest:
+Solver: David N. Campo
+Contact e-mail: dncampo at gmail dot com
+Solution remarks: none.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    unsigned long long a, b;
+
+    cin >> a >> b;
+    a--;
+    cout << (b*(b+1) - a*(a+1))/2 << endl;
+}
