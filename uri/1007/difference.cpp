9651fe6e48f7600ef30ec383cd97f456ad288b2a
David N Campo <dncampo@gmail.com>
2017-01-08 15:31:06 -0300

    URI 1007 Difference

---
+++ uri/1007/difference.cpp
@@ -0,0 +1,22 @@
+/*
+Title: Difference
+Tags: adhoc, basic
+URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+Resources of interest:
+Solver: David N. Campo
+Contact e-mail: dncampo at gmail dot com
+Solution remarks: none.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << "DIFERENCA = " << (a*b - c*d) << endl;
+    return 0;
+}
