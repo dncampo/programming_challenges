0c07bbe384dfc3c5dc731d182adbed9d183b17a5
David N Campo <dncampo@gmail.com>
2017-01-17 23:39:06 -0300

    URI 1016 Distance

---
+++ uri/1016/distance.cpp
@@ -0,0 +1,20 @@
+/*
+Title: Distance
+Tags: adhoc, basic
+URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1016
+Resources of interest:
+Solver: David N. Campo
+Contact e-mail: dncampo at gmail dot com
+Solution remarks: none.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    cout << (x<<1) << " minutos" << endl;
+    return 0;
+}
